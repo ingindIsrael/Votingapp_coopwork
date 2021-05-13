0c375258f99d35683ad17c85e7fb265b72946aed
Israel Vasquez <ing.visrael@gmail.com>
2021-05-13 22:35:19 +0000

    button SMS worked

--- src/front/js/component/nav.js
+++ src/front/js/component/nav.js
@@ -1,22 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import { Link, Redirect } from "react-router-dom";
+import "../../styles/tabNav.scss";
+import PropTypes from "prop-types";
 import { Navbar, Button, Form, FormControl, Nav, NavDropdown } from "react-bootstrap";
 
-export const NewNav = () => {
-	return (
-		<Navbar bg="dark clearfix">
-			<Navbar.Brand>
-				<i className="far fa-handshake" />
-			</Navbar.Brand>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="ml-auto">
-					<Nav.Link href="/accounts">Home</Nav.Link>
+export const NewNav = props => {
+	const { store, actions } = useContext(Context);
+	let isAdmin = sessionStorage.getItem("admin");
+	const navForAdmin = () => {
+		if (sessionStorage.getItem("admin")) {
+			console.log("estamos dentro del primer if TRUE");
+			return (
+				<>
 					<Nav.Link href="/createuser">Edit Users</Nav.Link>
 					<Nav.Link href="/createevent">Edit Events</Nav.Link>
 					<Nav.Link href="/chat">Set Debate</Nav.Link>
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
+				</>
+			);
+		} else if (!sessionStorage.getItem("admin")) {
+			console.log("estamos dentro del SEGUNDO if FALSE");
+			return null;
+		}
+	};
+
+	return (
+		<>
+			{/* {props.login ? ( */}
+			<Navbar className="NavWhite" variant="dark">
+				<Navbar.Brand>
+					<i className="far fa-handshake" />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ml-auto">
+						<Nav.Link href="/accounts">Home</Nav.Link>
+
+						<Nav.Link
+							href="/"
+							onClick={() => {
+								sessionStorage.clear();
+								setLogin(false);
+							}}>
+							Logout
+						</Nav.Link>
+						{console.log(sessionStorage.getItem("admin"))}
+						{navForAdmin()}
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			{/* // ) : ( // <Redirect to="/" />
+			)} */}
+		</>
 	);
+};
+NewNav.propTypes = {
+	login: PropTypes.bool,
+	setLogin: PropTypes.func
 };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
@@ -20,20 +20,18 @@ const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
+	const [login, setLogin] = useState(false);
 
 	return (
 		<div className="d-flex flex-column h-100">
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
-					<NewNav />
+					<NewNav login={login} setLogin={setLogin} />
 					{/* <TabNav /> */}
 					<Switch>
 						<Route exact path="/">
-							<Home />
+							<Home login={login} setLogin={setLogin} />
 						</Route>
-						{/* <Route exact path="/l">
-							<LineChart />
-						</Route> */}
 						<Route exact path="/accounts">
 							<Accounts />
 						</Route>
